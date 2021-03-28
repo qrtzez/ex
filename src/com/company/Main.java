32425af74f348631e606ef932c76a5e6b8e48293
qrtz <qrtz@mail.ru>
2021-03-28 23:22:07 +0300

    arrayInit2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,5 +9,6 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("1123");
 	List<String> list = new ArrayList<>();
+	List<String> list2 = new ArrayList<>();
     }
 }
